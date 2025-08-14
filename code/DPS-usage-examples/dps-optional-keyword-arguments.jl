64c8ec0ca1c12b7c2e1cf9a00b8bd72697c0a58e
chen-huaneng <huanengchen@foxmail.com>
2025-08-14 10:38:09 +0800

    Restructure the notes framework.

--- code/DPS-usage-examples/dps-optional-keyword-arguments.jl
+++
@@ -1,4 +0,0 @@
-n_groups::Int = 1, 
-method::String = "TSP-ep-all", 
-flying_range::Float64 = MAX_DRONE_RANGE, 
-time_limit::Float64 = MAX_TIME_LIMIT
--- code/DPS-usage-examples/dps-usage-example-a.jl
+++
@@ -1,9 +0,0 @@
-using TSPDrone
-n = 10 
-x = rand(n); y = rand(n);
-truck_cost_factor = 1.0 
-drone_cost_factor = 0.5
-result = solve_tspd(x, y, truck_cost_factor, drone_cost_factor)
-@show result.total_cost;
-@show result.truck_route;
-@show result.drone_route;
